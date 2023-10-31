6d50a41875863c0d01eefeb2545d156c497a218f
noelia gonzales <gonzalespereira7@gmail.com>
2023-10-31 17:51:42 -0400

    update

--- nave.py
+++ nave.py
@@ -1,4 +1,5 @@
 import pygame, random
+ 
 
 
 width=800
@@ -8,6 +9,7 @@ white=(255,255,255)
 blue = (0, 0, 255)
 pygame.init()
 pygame.mixer.init()
+
 screen=pygame.display.set_mode((width,height))
 pygame.display.set_caption("meteoros")
 clock= pygame.time.Clock()
@@ -16,7 +18,7 @@ clock= pygame.time.Clock()
 class Nave(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image=pygame.image.load("img/nave.png").convert()
+        self.image=pygame.transform.scale((pygame.image.load("img/nave.png").convert()),(50,50))
         self.image.set_colorkey(blue)
         self.rect=self.image.get_rect()
         self.rect.centerx = width//2
@@ -43,14 +45,18 @@ class Nave(pygame.sprite.Sprite):
         if self.rect.left<0:
             self.rect.left=0
         if self.rect.top<0:
-            self.rect.top=0
+            self.rect.top=0 
         if self.rect.bottom>height:
             self.rect.bottom=height
-
     def disparar(self):
-        bala=bala()
-        all_sprites.add(bala)
-        balas.add(bala)
+        bullet = Bala(self.rect.centerx,self.rect.top)
+        print("bala:",self.rect.centerx," ",self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
+        
+    
+
+
 class Meteorito(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -70,12 +76,32 @@ class Meteorito(pygame.sprite.Sprite):
            self.rect.y=random.randrange(-25,-10)
            self.speedy=random.randrange(1,3)
 
+    
 
-        
+class Bala(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        super().__init__()
+        #carga la imagen
+        self.image=pygame.image.load("img/laser1.png")
+        #remover el fondo
+        self.image.set_colorkey(black)
+        self.rect=self.image.get_rect()
+        self.rect.centerx=x
+        self.speedy=-10
+    def update(self):
+        self.rect.y+=self.speedy
+        if self.rect.bottom<0:
+            self.kill()
+    
+
+
+#cargar imagen de fondo
+#background=pygame.transform.scale((pygame.image.load("img/background.png").convert()),(100,100))      
 
 
 all_sprites=pygame.sprite.Group()
 meteoro_lista=pygame.sprite.Group()
+bullets=pygame.sprite.Group()
 
 jugador=Nave()
 all_sprites.add(jugador)
@@ -83,22 +109,32 @@ for i in range(8):
     meteoro=Meteorito()
     all_sprites.add(meteoro)
     meteoro_lista.add(meteoro)
+    
 
 runnig=True
 while runnig:
     clock.tick(60)
-    for event in pygame.event.get():
+    for event in pygame.event.get(): 
         if event.type==pygame.QUIT:
             runnig=False
         elif event.type==pygame.KEYDOWN:
             if event.key==pygame.K_SPACE:
                 jugador.disparar()
+
     all_sprites.update()
-    #colisiones
+    #colisiones-meteoro-laser
+    choque=pygame.sprite.groupcollide(meteoro_lista,bullets,True,True)
+    for choq in choque:
+        meteoro=Meteorito()
+        all_sprites.add(meteoro)
+        meteoro_lista.add(meteoro)
+    #colisiones-jugador-meteoro
     choque= pygame.sprite.spritecollide(jugador,meteoro_lista,True)
     if choque:
         runnig=False
-
+        
+    #screen.blit(background,[100,100])
+    
     screen.fill(black)
     all_sprites.draw(screen)
     pygame.display.flip()
